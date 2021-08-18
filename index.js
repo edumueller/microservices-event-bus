a672b30969284a8a34aa3e3d5431c03858e8a74c
edumueller <eduardo.nicastro@icloud.com>
2021-08-17 22:38:39 -0300

    feat: update addresses using k8s services

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.post("/events", (req, res) => {
   axios.post("http://posts-clusterip-srv:4000/events", event).catch((e) => {
     console.log(e);
   });
-  axios.post("http://localhost:4001/events", event).catch((e) => {
+  axios.post("http://comments-srv:4001/events", event).catch((e) => {
     console.log(e);
   });
-  axios.post("http://localhost:4002/events", event).catch((e) => {
+  axios.post("http://query-srv:4002/events", event).catch((e) => {
     console.log(e);
   });
-  axios.post("http://localhost:4003/events", event).catch((e) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((e) => {
     console.log(e);
   });
 
